2ff7cee2f582570f474bc466bf478edd9927f1b4
Demsity <simonbacklin@hotmail.com>
2023-04-19 15:40:50 +0200

    added admin dashboard GUI

---
+++ Assignment-ASP/Controllers/DashboardController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_ASP.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers() 
+        {
+            return View();
+        }
+
+        public IActionResult ManageProducts() 
+        {
+            return View();
+        }
+    }
+}
