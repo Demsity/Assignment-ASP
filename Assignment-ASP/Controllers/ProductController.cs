b74c364f0122c480f48c754b733152f0f38abc82
Demsity <simonbacklin@hotmail.com>
2023-04-27 11:06:25 +0200

    Added Details View and Route

--- Assignment-ASP/Controllers/ProductController.cs
+++ Assignment-ASP/Controllers/ProductController.cs
@@ -1,17 +1,40 @@
-﻿using Assignment_ASP.ViewModels;
+﻿using Assignment_ASP.Models;
+using Assignment_ASP.Services;
+using Assignment_ASP.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Assignment_ASP.Controllers
-{
-    public class ProductController : Controller
-    {
-        public ProductViewModel ViewModel = new();
+namespace Assignment_ASP.Controllers;
 
-        //SingleView
-        public IActionResult Index()
+public class ProductController : Controller
+{
+    private readonly ProductService _productService;
+
+    public ProductController(ProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public ProductViewModel ViewModel = new();
+
+    //SingleView
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Products";
+        return View(ViewModel);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        ViewData["Title"] = "Products - ";
+        if (id > 0) 
         {
-            ViewData["Title"] = "Products";
-            return View(ViewModel);
+            ProductModel _product = await _productService.GetProductById(id);
+            if (_product != null)
+            {
+                ViewData["Title"] = _product.Name;
+                return View(_product);
+            }
         }
+        return View("Index");
     }
 }
